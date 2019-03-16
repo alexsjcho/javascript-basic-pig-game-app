61a3bd0b7e70ec7eb425db4cdff81b514a25f3a5
Alex Cho <alexsjcho@gmail.com>
2019-03-15 18:13:09 -0700

    implemented HOLD function and DRY principle

--- app.js
+++ app.js
@@ -22,6 +22,21 @@ document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 
+//Next player game logic fucntion
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  //Toggle and change players
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  //Dice Styles when turn resets
+  document.querySelector(".dice").style.display = "none";
+}
+
 //Eventlistener logic on the Roll Dice element
 document.querySelector(".btn-roll").addEventListener("click", function() {
   var dice = Math.floor(Math.random() * 6) + 1;
@@ -32,17 +47,31 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     roundScore += dice;
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
   } else {
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScore = 0;
-    document.getElementById("current-0").textContent = "0";
-    document.getElementById("current-1").textContent = "0";
+    nextPlayer();
+  }
+});
 
-    //Toggle and change players
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  //Add CURRENT score to GLOBAL score
+  scores[activePlayer] += roundScore;
 
-    //Dice Styles when turn resets
+  //Update the UI
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  //Check if PLAYER won the game
+  if (scores[activePlayer] >= 10) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
     document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("winner");
+  } else {
+    //Next Player logic
+    nextPlayer();
   }
 });
 
