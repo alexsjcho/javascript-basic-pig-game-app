a47594b293eb972c772acf39083a618a6d34bb1e
Alex Cho <alexsjcho@gmail.com>
2019-03-15 17:46:22 -0700

    added event and event handling logic for rolling dice

--- app.js
+++ app.js
@@ -8,12 +8,29 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-document.querySelector("#current-" + activePlayer).innerHTML =
-  "<em>" + dice + "</em>";
+//Hide dice on initial page load
 document.querySelector(".dice").style.display = "none";
+
+//Set all initial score to 0
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+//Eventlistener logic on the Roll Dice element
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var dice = Math.floor(Math.random() * 6) + 1;
+  var diceDom = document.querySelector(".dice");
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + dice + ".png";
+});
+
+// dice = Math.floor(Math.random() * 6) + 1;
+
+// document.querySelector("#current-" + activePlayer).innerHTML =
+//   "<em>" + dice + "</em>";
